79ae0fbdd5294f271eec9071eac1a8c1c024dda8
Havva Gülsüm Özler <havvagulsumozler@gmail.com>
2025-12-21 18:44:36 +0300

    Add files via upload

---
+++ Hafta-9-Slayt/Hafta 9 Slayt/Program.cs
@@ -0,0 +1,83 @@
+﻿namespace Hafta_9_Slayt
+{
+    internal class Program
+    {
+        static int[] fiyatlar = { 30, 10, 5 };
+        static string[] urunler = { "Tost", "Ayran", "Çay" };
+        static void MenuGoster()
+        {
+            Console.WriteLine("1 - Tost (30 TL)");
+            Console.WriteLine("2 - Ayran (10 TL)");
+            Console.WriteLine("3 - Çay (5 TL)");
+        }
+        static int SecimAl()
+        {
+            Console.WriteLine("Seçiminizi yapınız(1, 2, 3):");
+            string secim = Console.ReadLine();
+
+            while (secim != "1" && secim != "2" && secim != "3")
+            {
+                Console.WriteLine("Yanlış seçim yaptınuz, lütfen tekrar deneyiniz.");
+                secim = Console.ReadLine();
+            }
+            return Convert.ToInt32(secim);
+        }
+        static int AdetAl()
+        {
+            Console.WriteLine("Adet sayısını giriniz:");
+            int adet = Convert.ToInt32(Console.ReadLine());
+            while (adet < 0)
+            {
+                Console.WriteLine("Hatalı giriş,lütfen tekrar giriniz.");
+                adet = Convert.ToInt32(Console.ReadLine());
+            }
+            return adet;
+        }
+        static int FiyatGetir(int urunKodu)
+        {
+            return fiyatlar[urunKodu - 1];
+        }
+        static int SatırTutarıHesapla(int adet, int birimFiyat)
+        {
+            return adet * birimFiyat;
+        }
+        static bool IndirimHakkiVarMi(int toplamTutar)
+        {
+            if (toplamTutar > 100)
+                return true;
+            else
+                return false;
+        }
+        static double IndirimliTutarıHesapla(int toplamTutar)
+        {
+            if (IndirimHakkiVarMi(toplamTutar))
+            {
+                return toplamTutar * 0.9;
+            }
+            else
+                return toplamTutar;
+        }
+        static void Yazdır(int secim, int adet, int fiyat, int toplamTutar, bool indirimVarMi, double sonTutar)
+        {
+            Console.WriteLine("Müşterinin seçtiği ürün :");
+            Console.WriteLine("Ürünün adı :" + urunler[secim - 1]);
+            Console.WriteLine("Adet :" + adet);
+            Console.WriteLine("Fiyat :" + fiyat);
+            Console.WriteLine("Toplam Tutar :" + toplamTutar);
+            Console.WriteLine("İndirim var mı " + indirimVarMi);
+        }
+        static void Main(string[] args)
+        {
+            MenuGoster();
+            Console.WriteLine("-------------------");
+            int secim = SecimAl();
+            int adet = AdetAl();
+            int fiyat = FiyatGetir(secim);
+            int toplamTutar = SatırTutarıHesapla(adet, fiyat);
+            double sonTutar = IndirimliTutarıHesapla(toplamTutar);
+            bool indirimVarMi = IndirimHakkiVarMi(toplamTutar);
+            Yazdır(secim, adet, fiyat, toplamTutar, indirimVarMi, sonTutar);
+
+        }
+    }
+}
---
+++ Hafta-9-Slayt/Soru2/Program.cs
@@ -0,0 +1,115 @@
+﻿namespace Soru2
+{
+    internal class Program
+    {
+        static string[] esyalar = { "Kitap", "Laptop", "Telefon", "Kamera", "Tablet" };
+        static int[] agırlıklar = { 5, 25, 10, 27, 17 };
+        static string[] esyalar2 = new string[esyalar.Length + 1];
+        static int[] agırlıklar2 = new int[agırlıklar.Length + 1];
+        static void EsyaEkle(string item, int agırlık)
+        {
+            for (int i = 0; i < esyalar.Length; i++)
+            {
+                esyalar2[i] = esyalar[i];
+                agırlıklar2[i] = agırlıklar[i];
+            }
+            esyalar2[esyalar2.Length - 1] = item;
+            agırlıklar2[agırlıklar2.Length - 1] = agırlık;
+
+        }
+        static void EsyaSil(string item2)
+        {
+            for (int i = 0; i < esyalar2.Length; i++)
+            {
+                if (esyalar2[i] == item2)
+                {
+                    esyalar2[i] = null;
+                    agırlıklar[i] = 0;
+                    break;
+                }
+                else
+                {
+                    Console.WriteLine("Silmek istediğiniz eşya bulunamadı.");
+                }
+            }
+
+        }
+        static void esyaListele()
+        {
+            for (int i = 0; i < esyalar2.Length; i++)
+            {
+                if (esyalar2[i] != null)
+                {
+                    Console.WriteLine($"{esyalar2[i]} - {agırlıklar2[i]} kg");
+                }
+            }
+        }
+        static int esyalarınToplamAgırlığı()
+        {
+            int toplam = 0;
+            for (int i = 0; i < agırlıklar2.Length; i++)
+            {
+                toplam += agırlıklar2[i];
+            }
+            return toplam;
+        }
+        static int enAgırBul()
+        {
+            int enAgır = agırlıklar2[0];
+            for (int i = 1; i < agırlıklar2.Length; i++)
+            {
+                if (agırlıklar2[i] > enAgır)
+                {
+                    enAgır = agırlıklar2[i];
+                }
+            }
+            return enAgır;
+        }
+        static int enHafifBul()
+        {
+            int enHafif = agırlıklar2[0];
+            for (int i = 1; i < agırlıklar2.Length; i++)
+            {
+                if (agırlıklar2[i] < enHafif)
+                {
+                    enHafif = agırlıklar2[i];
+                }
+            }
+            return enHafif;
+        }
+        static void EsyaAra(string arananEsya)
+        {
+            bool bulundu = false;
+            for (int i = 0; i < esyalar2.Length; i++)
+            {
+                if (esyalar2[i] == arananEsya)
+                {
+                    Console.WriteLine($"{arananEsya} bulundu ve ağırlığı {agırlıklar2[i]} kg.");
+                    bulundu = true;
+                    break;
+                }
+            }
+        }
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Eklmek istediğiniz eşyayı giriniz:");
+            string item = Console.ReadLine();
+            Console.WriteLine("Eklediğiniz eşyanın ağırlığını giriniz:");
+            int agırlık = Convert.ToInt32(Console.ReadLine());
+            EsyaEkle(item, agırlık);
+            Console.WriteLine("Silmek istediğiniz eşyayı giriniz:");
+            string item2 = Console.ReadLine();
+            EsyaSil(item2);
+            esyaListele();
+            int toplamAgırlık = esyalarınToplamAgırlığı();
+            Console.WriteLine($"Esyaların toplam ağırlığı {toplamAgırlık} tır.");
+            int enAgır = enAgırBul();
+            Console.WriteLine($"En ağır eşya {enAgır} kg ağırlığındadır.");
+            int enHafif = enHafifBul();
+            Console.WriteLine($"En hafif eşya {enHafif} kg ağırlığındadır.");
+            Console.WriteLine("Aramak istediğiniz eşyayı giriniz:");
+            string arananEsya = Console.ReadLine();
+            EsyaAra(arananEsya);
+        }
+    }
+}
